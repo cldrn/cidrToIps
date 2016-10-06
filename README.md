edd9ecc715101014ff0705646d6b95bde2d140aa
cldrn <paulino@calderonpale.com>
2016-10-05 22:14:39 -0500

    More doc

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # cidrToIps
-Takes a list of targets in CIDR notation and transforms them into individual IPs
+This simple script reads a list of IP ranges in CIDR notation and prints the individual IP addresses.
+
+Posted this script because I got tired of re-writing the same script everytime I needed this. If you write security tools, please support CIDR notation and make my life easier.
+
+Usage: ./cidrToIps.py -i <inputfile>
+
+$ cat cidr-list.txt
+192.168.0.1/29
+192.168.1.1/30
+$ ./cidrToIps.py -i cidr-list.txt
+Reading IP ranges in CIDR notation from file: cidr-list.txt
+192.168.0.0
+192.168.0.1
+192.168.0.2
+192.168.0.3
+192.168.0.4
+192.168.0.5
+192.168.0.6
+192.168.0.7
+192.168.1.0
+192.168.1.1
+192.168.1.2
+192.168.1.3
+
+calderon@websec.mx
+
+
--- cidrToIps.py
+++ cidrToIps.py
@@ -6,7 +6,27 @@
 # everytime I needed this. If you write security tools, please support 
 # CIDR notation.
 #
-#Usage: cidrToIps.py -i <inputfile>
+#Usage: ./cidrToIps.py -i <inputfile>
+#
+#$ cat cidr-list.txt 
+#192.168.0.1/29
+#192.168.1.1/30
+#$ ./cidrToIps.py -i cidr-list.txt
+#Reading IP ranges in CIDR notation from file: cidr-list.txt
+#192.168.0.0
+#192.168.0.1
+#192.168.0.2
+#192.168.0.3
+#192.168.0.4
+#192.168.0.5
+#192.168.0.6
+#192.168.0.7
+#192.168.1.0
+#192.168.1.1
+#192.168.1.2
+#192.168.1.3
+#
+#calderon@websec.mx
 #
 
 from netaddr import IPNetwork
@@ -17,7 +37,6 @@ def main(argv):
   inputfile = ''
   try:
     opts, args = getopt.getopt(argv,"i:",["ifile="])
-    print opts, args
   except getopt.GetoptError:
     print 'cidrToIps.py -i <inputfile>'
     sys.exit(2)
